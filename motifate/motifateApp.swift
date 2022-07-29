6b66309a1d93f62b0146168c2025e943e14863b4
Sajan Pirithiviraj <pirithivirajsajan@gmail.com>
2022-07-29 19:46:27 -0400

    Initial Commit

---
+++ motifate/motifateApp.swift
@@ -0,0 +1,17 @@
+//
+//  motifateApp.swift
+//  motifate
+//
+//  Created by CoopStudent on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct motifateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
